5687fb5c41d52b2c768c26f3bd8cfc26b70b8f00
Alec Nghia <hieunghiabeo@gmail.com>
2024-07-16 13:01:05 +0700

    Fix security leaked

--- hotel-manager/backend/__init__.py
+++ hotel-manager/backend/__init__.py
@@ -18,7 +18,7 @@ def create_app():
     app.config['MAIl_PORT'] = 587
     app.config['MAIL_USE_TLS'] = True
     app.config['MAIL_USERNAME'] = 'hieunghiabeo@gmail.com'
-    app.config['MAIL_PASSWORD'] = 'shadowkiller297' 
+    app.config['MAIL_PASSWORD'] = '' 
     app.secret_key = 'miralec2629'
 
     db.init_app(app)
