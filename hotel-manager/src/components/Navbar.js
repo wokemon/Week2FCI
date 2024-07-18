d8c3f482b6858e5a935e99c333cc56a16889e2b7
Alec Nghia <hieunghiabeo@gmail.com>
2024-07-18 10:33:34 +0700

    Add Navbar

---
+++ hotel-manager/src/components/Navbar.js
@@ -0,0 +1,34 @@
+import * as React from 'react'; 
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Link as RouterLink } from 'react-router-dom';
+
+export default function AppNav() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" sx={{ flexGrow: 1, textDecoration: 'none'}}>
+            Welcome to the website
+          </Typography>
+          <Button color="inherit" component= {RouterLink} to="/login">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
+
