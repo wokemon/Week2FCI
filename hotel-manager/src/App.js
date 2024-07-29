d88e8dcfb86b16b6694f49fa3e6765e0c47baf0b
Alec Nghia <hieunghiabeo@gmail.com>
2024-07-29 15:25:12 +0700

    Add language switcher

--- hotel-manager/src/App.js
+++ hotel-manager/src/App.js
@@ -28,10 +28,13 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SignIn from './components/Login';
 import Landing from './components/Landing';
+import './i18n'; //init this
+import LanguageSwitcher from './components/LanguageSwitcher';
 
 export default function App() {
   return (
     <BrowserRouter>
+    <LanguageSwitcher></LanguageSwitcher>
     <Routes>
       <Route path='/' element={<Landing/>}></Route>
       <Route path='/login' element={<SignIn/>}></Route>
