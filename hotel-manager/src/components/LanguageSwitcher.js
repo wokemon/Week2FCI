a141a1fc1149e35884fabe4138eeaca3c6c5c36e
Alec Nghia <hieunghiabeo@gmail.com>
2024-08-01 13:51:06 +0700

    Add a drop down menu to choose languages

--- hotel-manager/src/components/LanguageSwitcher.js
+++ hotel-manager/src/components/LanguageSwitcher.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Button, ButtonGroup } from '@mui/material';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
 
 function LanguageSwitcher() {
   const { i18n } = useTranslation();
@@ -9,13 +12,32 @@ function LanguageSwitcher() {
     i18n.changeLanguage(lang);
   };
 
-  return (
-    <ButtonGroup variant="contained" aria-label="outlined primary button group">
-      <Button onClick={() => changeLanguage('en')}>English</Button>
-      <Button onClick={() => changeLanguage('vi')}>Tiếng Việt</Button>
-    </ButtonGroup>
+  const handleClick = (lang, popupState) => {
+    changeLanguage(lang);
+    popupState.close();
+  }
 
-    
+
+  return (
+    // <ButtonGroup variant="contained" aria-label="outlined primary button group">
+    //   <Button onClick={() => changeLanguage('en')}>English</Button>
+    //   <Button onClick={() => changeLanguage('vi')}>Tiếng Việt</Button>
+    // </ButtonGroup>
+
+    <PopupState variant="popover" popupId="demo-popup-menu">
+      {(popupState) => (
+        <React.Fragment>
+          <Button variant="contained" {...bindTrigger(popupState)}>
+            Dashboard
+          </Button>
+          <Menu {...bindMenu(popupState)}>
+            <MenuItem onClick={() => handleClick('en', popupState)}>English</MenuItem>
+            <MenuItem onClick={() => handleClick('vn', popupState)}>Vietnamese</MenuItem>
+            <MenuItem onClick={() => handleClick('es', popupState)}>Espanol</MenuItem>
+          </Menu>
+        </React.Fragment>
+      )}
+    </PopupState>
   );
 }
 
