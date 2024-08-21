d07b2c3e88e8354665918814d4216fc0e3be9bb7
Alec Nghia <hieunghiabeo@gmail.com>
2024-08-21 10:43:27 +0700

    Revamp database

--- hotel-manager/backend/models.py
+++ hotel-manager/backend/models.py
@@ -1,20 +1,21 @@
 from . import db
+import datetime
+from sqlalchemy import DateTime
 from werkzeug.security import generate_password_hash
 
 class User(db.Model):
-    __tablename__ = "User"
+    __tablename__ = "user"
     
-    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
-    role_id = db.Column(db.Integer, db.ForeignKey('Role.id'), nullable=False)
+    role_id = db.Column(db.Integer, db.ForeignKey('role.role_id'),unique=False, nullable=False)
+    created_at = db.Column(DateTime, default=datetime.datetime.now)
+    updated_at = db.Column(DateTime, default=datetime.now, onupdate=datetime.now)
     
-    role = db.relationship("Role", backref="users")
-    
-    def has_permission(self, permission_name):
-        return any(permission.name == permission_name for permission in self.role.permissions)
-    
+    roles = db.relationship('role', backref='user')
+
     def __init__ (self, username, mail, password, role_id):
         self.username = username
         self.email = mail
@@ -22,48 +23,75 @@ class User(db.Model):
         self.role_id = role_id
     
 class Role(db.Model):
-    __tablename__ = "Role"
+    __tablename__ = "role"
     #Roles: owner(hotel manager), concierge, admin office(admins/operators), F&B/Sommerlier/Tailor/Cartographer/Mechanic
     
-    id = db.Column(db.Integer, primary_key=True)
-    role = db.Column(db.String(80), unique=True, nullable=False)
-    permissions = db.relationship("Permission", backref='role', lazy=True)
+    role_id = db.Column(db.Integer, primary_key=True)
+    role_name = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.Column(db.String(255), nullable=True)
+
+    #one to one with role_permission
+    role = db.relationship('role_permission', backref='role', uselist=False)
     
 class Permission(db.Model):
-    __tablename__ = "Permission"
-    
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    role_id = db.Column(db.Integer, db.ForeignKey('Role.id'), nullable=False)
+    __tablename__ = "permission"
+    permission_id = db.Column(db.Integer, primary_key=True)
+    permission_name = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.Column(db.String(255), nullable=True)
+
+    #one to one with role_permission
+    permission = db.relationship('role_permission', backref='permission', uselist=False)
+
+class Role_permission(db.Model):
+    __tablename__ = "role permission"
+
+    role_permission_id = db.Column(db.Integer, primary_key=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('role.role_id'))
+    permission_id = db.Column(db.Integer, db.ForeignKey('permission.permission_id'))
 
 class Reservation(db.Model):
-    __tablename__ = "Reservation"
+    __tablename__ = "reservation"
     
-    guest_id = db.Column(db.Integer, primary_key=True)
-    guest_first_name = db.Column(db.String(80), nullable=False)
-    guest_last_name = db.Column(db.String(80), nullable=False)
-    number_of_guest = db.Column(db.String(255), nullable=False)
-    guest_phone = db.Column(db.String(255), unique=True, nullable=False)
-    guest_email = db.Column(db.String(80), unique=True, nullable=False)
-    room_id = db.Column(db.Integer, db.ForeignKey('Room.room_id'), nullable=False)
+    reservation_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
+    room_id = db.Column(db.Integer, db.ForeignKey('room.room_id'), nullable=False)
+    check_in_date = db.Column(DateTime, default=datetime.datetime.now)
+    check_out_date = db.Column(DateTime, default=datetime.now, onupdate=datetime.now)
+    status = db.Column(db.String(100), nullable=False, unique=False)
+    total_price = db.Column(db.Decimal, nullable=False)
+    created_at = db.Column(DateTime, default=datetime.datetime.now)
+    updated_at = db.Column(DateTime, default=datetime.now, onupdate=datetime.now)
     
 class Room(db.Model):
-    __tablename__ = "Room"
+    __tablename__ = "room"
     
     room_id = db.Column(db.Integer, primary_key=True)
     #Number from 1-6
-    room_number = db.Column(db.String(80), unique=True, nullable=False)
+    room_number = db.Column(db.Integer, unique=True, nullable=False)
     #Type: Pawn, Rook, Queen, King
     room_type = db.Column(db.String(80), nullable=False)
-    availability = db.Column(db.Boolean, unique=False, default=True)
-    service_id = db.Column(db.Integer, db.ForeignKey('Service.service_id'), nullable=False)
+    status = db.Column(db.Boolean, unique=False)
+    price = db.Column(db.Decimal, nullable=False)
+    description = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(DateTime, default=datetime.datetime.now)
+    updated_at = db.Column(DateTime, default=datetime.now, onupdate=datetime.now)
     
 class Service(db.Model):
-    __tablename__ = "Service"
+    __tablename__ = "service"
     
     service_id = db.Column(db.Integer, primary_key=True)
     #Type: Food, beverage, tailor, laundry, cleaning, valet
-    type = db.Column(db.String(80), unique=True, nullable=False)
-    availability = db.Column(db.Boolean, unique=False, default=True)
+    service_name = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.Column(db.String(255), unique=False)
+    price = db.Column(db.Decimal, nullable=False)
+
+class Service_request(db.Model):
+    __tablename__ = "service request"
     
-    
+    request_id = db.Column(db.Integer, primary_key=True)
+    service_id = db.Column(db.Integer, db.ForeignKey('service.sevice_id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
+    room_id = db.Column(db.Integer, db.ForeignKey('room.room_id'), nullable=False)
+    status = db.Column(db.String(255), unique=True, nullable=False)
+    created_at = db.Column(DateTime, default=datetime.datetime.now)
+    updated_at = db.Column(DateTime, default=datetime.now, onupdate=datetime.now)
