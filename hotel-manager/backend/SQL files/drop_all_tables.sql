50885e907a9d721524d74b3230ea29b76bcfba4e
Alec Nghia <hieunghiabeo@gmail.com>
2024-08-22 14:04:09 +0700

    Add drawer to the frontpage

--- hotel-manager/backend/SQL files/drop_all_tables.sql
+++ hotel-manager/backend/SQL files/drop_all_tables.sql
@@ -5,4 +5,7 @@ DROP TABLE hotelmanager.room;
 DROP TABLE hotelmanager.service;
 DROP TABLE hotelmanager.permission;
 DROP TABLE hotelmanager.user;
+DROP TABLE hotelmanager.service_request;
+DROP TABLE hotelmanager.role_permission;
+DROP TABLE hotelmanager.audit_log;
 SET FOREIGN_KEY_CHECKS = 1;
--- hotel-manager/src/components/Navbar.js
+++ hotel-manager/src/components/Navbar.js
@@ -8,28 +8,67 @@ import { createTheme, Stack } from '@mui/material';
 import { ThemeProvider } from '@emotion/react';
 import { Link as RouterLink } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import i18next from 'i18next'
+import HomeIcon from '@mui/icons-material/Home';
+import RoomServiceIcon from '@mui/icons-material/RoomService';
 
 const theme = createTheme({
   palette: {
     color: {
-      text: "white"
+      text: "black"
     }
   }
 });
 
 export default function AppNav() {
+  const [open, setOpen] = React.useState(false);
+
+  const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen);
+  };
+
   const { t } = useTranslation();
+
+  const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+      <List>
+        <ListItem disablePadding component= {RouterLink} to='/'>
+          <ListItemIcon>
+            <HomeIcon/>
+          </ListItemIcon>
+          <ListItemText primary={i18next.t('home')}/>
+        </ListItem>
+      </List>
+      <List>
+        <ListItem disablePadding component= {RouterLink} to='/service'>
+          <ListItemIcon>
+            <RoomServiceIcon/>
+          </ListItemIcon>
+          <ListItemText primary={i18next.t('services')}/>
+        </ListItem>
+      </List>
+      <Divider />
+    </Box>
+  );
+
   return (
     <ThemeProvider theme={theme}>
       <Box sx={{ flexGrow: 1, border: 0 }}>
       <AppBar position="static" sx={{background: 'transparent', boxShadow: 'none'}}>
         <Toolbar>
           <Typography variant="h4" sx={{ flexGrow: 1}} color={'#1976D2'}>
-            
+            <Button onClick={toggleDrawer(true)} sx={{ color: 'color.text'}}>Menu</Button>
+              <Drawer open={open} onClose={toggleDrawer(false)}>
+                {DrawerList}
+              </Drawer>
           </Typography>
           <Stack direction={{xs: 'column', md: 'row'}} spacing={'0.5'} alignContent={'flex-end'}>
-            <Button sx={{ color: 'color.text'}} variant='text' component= {RouterLink} to='/'>{t('home')}</Button>
-            <Button sx={{ color: 'color.text'}} variant='text' component= {RouterLink} to="/service">{t('services')}</Button>
             <Button sx={{ color: 'color.text'}} variant='text' component= {RouterLink} to="/login/signup">{t('sign_up')}</Button>
             <Button sx={{ color: 'color.text'}} variant='text' component= {RouterLink} to="/login">{t('sign_in')}</Button>
           </Stack>
@@ -39,4 +78,3 @@ export default function AppNav() {
     </ThemeProvider>
   );
 }
-
